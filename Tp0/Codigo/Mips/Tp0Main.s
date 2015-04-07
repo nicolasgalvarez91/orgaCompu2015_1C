c537b2e9116d8cbbae4fcac917f617883da3eee0
npfernandeztheillet <nflabo@gmail.com>
2015-04-07 17:43:43 -0300

    .s faltantes

---
+++ Tp0/Codigo/Mips/Tp0Main.s
@@ -0,0 +1,46 @@
+	.file	1 "Tp0Main.c"
+	.section .mdebug.abi32
+	.previous
+	.abicalls
+	.text
+	.align	2
+	.globl	main
+	.ent	main
+main:
+	.frame	$fp,48,$ra		# vars= 8, regs= 3/0, args= 16, extra= 8
+	.mask	0xd0000000,-8
+	.fmask	0x00000000,0
+	.set	noreorder
+	.cpload	$t9
+	.set	reorder
+	subu	$sp,$sp,48
+	.cprestore 16
+	sw	$ra,40($sp)
+	sw	$fp,36($sp)
+	sw	$gp,32($sp)
+	move	$fp,$sp
+	sw	$a0,48($fp)
+	sw	$a1,52($fp)
+	lw	$a0,48($fp)
+	lw	$a1,52($fp)
+	la	$t9,checkArguments
+	jal	$ra,$t9
+	bne	$v0,$zero,$L18
+	sw	$zero,24($fp)
+	b	$L17
+$L18:
+	lw	$a0,48($fp)
+	lw	$a1,52($fp)
+	la	$t9,manejoArchivos
+	jal	$ra,$t9
+	sw	$v0,24($fp)
+$L17:
+	lw	$v0,24($fp)
+	move	$sp,$fp
+	lw	$ra,40($sp)
+	lw	$fp,36($sp)
+	addu	$sp,$sp,48
+	j	$ra
+	.end	main
+	.size	main, .-main
+	.ident	"GCC: (GNU) 3.3.3 (NetBSD nb3 20040520)"
