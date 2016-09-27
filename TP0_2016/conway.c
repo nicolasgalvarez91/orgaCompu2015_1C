f7312b0b6c771b570ac0365766d8e3c668fc9dd7
Gonzalo Merino <merino.gonza@gmail.com>
2016-09-27 03:41:25 -0300

    Actualizado con getopt. Falta opcion -o para nombre de archivo de salida.

--- TP0_2016/conway.c
+++ TP0_2016/conway.c
@@ -1,15 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <string.h>
 
 int main( int argc, char *argv[] )  {
 	//argc = nro de argumentos, 2 significa 1, no se porque
 	// argv contiene los argumentos en un array
 
-   if( argc == 5 ) {
-   	//printf("The argument supplied is %s\n", argv[1]);
-   	//Opcion valida -> 4 argumentos: i, M, N, NombreArchivo
+   int c;
 
+   char* help = "Uso:\n"
+            "\tconway -h\n"
+            "\tconway -V\n"
+            "\tconway i M N inputfile [-o outputprefix]\n"
+            "Opciones:\n"
+            "\t-h, --help\tImprime este mensaje.\n"
+            "\t-V, --version\tDa la version del programa.\n"
+            "\t-o Prefijo de los archivos de salida.\n"
+            "Ejemplos:\n"
+            "\tconway 10 20 20 glider -o estado\n"
+            "\tRepresenta 10 iteraciones del Juego de la Vida en una matriz de 20x20,\n"
+            "\tcon un estado inicial tomado del archivo glider.\n"
+            "\tLos archivos de salida se llamaran estado_n.pbm.\n"
+            "\tSi no se da un prefijo para los archivos de salida,\n"
+            "\tel prefijo sera el nombre del archivo de entrada.\n";
+
+
+   if ((argc == 2)
+         && ((strcmp(argv[1], "-h") == 0)
+               || (strcmp(argv[1], "--help") == 0))) {
+      printf("%s", help);
+   } else if ((argc == 2)
+         && ((strcmp(argv[1], "-V") == 0)
+               || (strcmp(argv[1], "--version") == 0))) {
+      printf("Conway - Version 1.0\n");
+   } else if (argc == 5) {
    	int filas = atoi(argv[2]);
    	int columnas = atoi(argv[3]);	
 
@@ -29,7 +54,7 @@ int main( int argc, char *argv[] )  {
    		}
    	}
 
-  	FILE *fp;
+  	   FILE *fp;
    	char fila[5];
    	char columna[5];
 
@@ -39,17 +64,11 @@ int main( int argc, char *argv[] )  {
    	}
    	fclose(fp);
 
-	for (int i = 0; i<filas; i++) {
-		for (int j = 0; j<columnas; j++) {
-   			printf("%d ", celdas[i][j]);
-   		}
-   		printf("\n");
-   	}   	
-   }
-   else if( argc > 5 ) {
-      printf("Sobran argumentos.\n");
-   }
-   else {
-      printf("Faltan argumentos.\n");
+   	for (int i = 0; i<filas; i++) {
+   		for (int j = 0; j<columnas; j++) {
+      		printf("%d ", celdas[i][j]);
+      	}
+      	printf("\n");
+      }   	
    }
 }
